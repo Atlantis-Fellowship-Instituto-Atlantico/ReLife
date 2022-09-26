ab43e88ed83f3c9c074c029e08f96d3ab6b33336
kasilianaoliveira <kasilianaoliveira@gmail.com>
2022-09-26 20:07:53 -0300

    Initial project setup

---
+++ src/server.ts
@@ -0,0 +1,9 @@
+import express from "express";
+
+const app = express();
+
+app.get("/teste", (request, response) => {
+  return response.json([{ id: 1, name: "teste" }]);
+});
+
+app.listen(3333, () => console.log("Server is running"));
