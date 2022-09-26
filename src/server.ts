cf6a4a825c52af181a808c9cbfd437aad4188d66
Gabriel F Albuquerque <gfonteles.a@gmail.com>
2022-09-26 20:49:54 -0300

    feature: criacao endpoint usuario

--- src/server.ts
+++ src/server.ts
@@ -8,4 +8,4 @@ app.get("/teste", (request, response) => {
 
 app.listen(3333, () => console.log("Server is running"));
 
-//feature dfsdfdfs blac k
+//feature dfsdfdfs blac k uysauhsu huhsau hshahus
